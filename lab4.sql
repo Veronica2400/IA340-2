b86fdf55aa256fd56678a05410fca4aac9297a5b
Veronica2400 <Veronica.L14@outlook.com>
2024-09-23 16:06:34 -0400

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -6,12 +6,12 @@ this is because it is referred in the other table. */
 -- 4.2
 
 /* No,
-we cannot execute the provided code because the fips '80' key does not exist in either table */
+we cannot execute the provided code because the fips '80' key does not exist in either table. */
 
 -- 4.3
 
 /* Yes
-It is perfect */
+It is perfect. */
 
 -- 4.5
 
@@ -20,7 +20,6 @@ chatgpt does recognize the er diagram. */
 
 -- 4.6
 
-/* Code:
 SELECT n.name, i.fips, i.year, i.income
 FROM income i
 JOIN name n ON i.fips = n.fips
@@ -28,8 +27,8 @@ WHERE i.year = (SELECT MAX(year) FROM income)
 ORDER BY i.income DESC
 LIMIT 1;
 
-Yes,
-the code worked successfully */
+/*Yes,
+the code worked successfully. */
 
 -- 4.7
 
